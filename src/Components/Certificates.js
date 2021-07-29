cf75b59c4c1225ba9eb43a2eb9ad05b8b035b6cd
narzikulovv <sardornarzikulov2002@gmail.com>
2021-07-29 23:47:49 +0500

    news build component

---
+++ src/Components/Certificates.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Certificates = () => {
+    return (
+        <div className="certificates_main">
+            <div className="container cer_con">
+                <div className="row cer_row">
+                    <div className="col-12 cer_col12">
+                        <h1>Certificates</h1>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4 cer_col4">
+                        <div className="card cer_card">
+                            <div className="card-header">
+                                <h1>Firma guvohnomasi</h1>
+                            </div>
+                            <div className="card-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores aspernatur
+                                    inventore maxime nam nisi officia, provident quod sequi tempore?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4  cer_col4">
+                        <div className="card cer_card">
+                            <div className="card-header">
+                                <h1>Firma guvohnomasi</h1>
+                            </div>
+                            <div className="card-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores aspernatur
+                                    inventore maxime nam nisi officia, provident quod sequi tempore?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4 cer_col4">
+                        <div className="card cer_card">
+                            <div className="card-header">
+                                <h1>Firma guvohnomasi</h1>
+                            </div>
+                            <div className="card-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores aspernatur
+                                    inventore maxime nam nisi officia, provident quod sequi tempore?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Certificates;
--- src/Components/Programs.js
+++ src/Components/Programs.js
@@ -7,31 +7,31 @@ const Programs = () => {
         <div className="programs_main">
             <div className="container con_programs">
                 <div className="row row_programs">
-                    <div className="col-12">
+                    <div className="col-12 col12_programs">
                         <h1 className="text-center">About us</h1>
                     </div>
-                    <div className="col-6">
-                        <div className="card">
+                    <div className="col-12 col-md-12 col-sm-12 col-lg-6 col6_programs">
+                        <div className="card card_programs">
                             <div className="card-header">
                                 <h1>Our Mission</h1>
                             </div>
                             <div className="card-body">
-                                <h4><span><GiCheckMark/></span> Lorem</h4>
-                                <h4><span><GiCheckMark/></span> Lorem</h4>
-                                <h4><span><GiCheckMark/></span> Lorem</h4>
+                                <h4><span><GiCheckMark/></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, placeat.</h4>
+                                <h4><span><GiCheckMark/></span> Lorem ipsum dolor sit amet.</h4>
+                                <h4><span><GiCheckMark/></span> Lorem ipsum dolor sit amet, consectetur.</h4>
                                 <h4><span><GiCheckMark/></span> Lorem</h4>
                             </div>
                         </div>
                     </div>
-                    <div className="col-6">
-                        <div className="card">
+                    <div className="col-12 col-md-12 col-sm-12 col-lg-6 col6_programs">
+                        <div className="card card_programs">
                             <div className="card-header">
                                 <h1>Our Vision</h1>
                             </div>
                             <div className="card-body">
-                                <h4><span><GiCheckMark/></span> Lorem</h4>
-                                <h4><span><GiCheckMark/></span> Lorem</h4>
-                                <h4><span><GiCheckMark/></span> Lorem</h4>
+                                <h4><span><GiCheckMark/></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, placeat.</h4>
+                                <h4><span><GiCheckMark/></span> Lorem ipsum dolor sit amet.</h4>
+                                <h4><span><GiCheckMark/></span> Lorem ipsum dolor sit amet, consectetur.</h4>
                                 <h4><span><GiCheckMark/></span> Lorem</h4>
                             </div>
                         </div>
